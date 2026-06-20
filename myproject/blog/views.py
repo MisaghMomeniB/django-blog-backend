2adb0bf1066a845255810b1cf42874765fb37b0a
MisaghMomeniB <jenabemisagh@gmail.com>
2026-06-20 11:59:25 +0330

    define request for handle view

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def home(request):
+    all_posts = Post.objects.all().order_by("-created_at")
+    return render(request, "blog/home.html", {"posts": all_posts})
