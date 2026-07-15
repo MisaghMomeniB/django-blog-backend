12f38ec273f108715e0e07af6e2e11b4ce8c1c22
MisaghMomeniB <jenabemisagh@gmail.com>
2026-07-15 11:59:25 +0330

    define new def for post detail with pk

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
-def home(request):
+def post_list(request):
     all_posts = Post.objects.all().order_by("-created_at")
     return render(request, "blog/post_list.html", {"posts": all_posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
